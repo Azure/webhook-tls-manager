15940f4345655214106e7f6235792355fbfdf6c2
zhifanhui <zhifanhui@microsoft.com>
2025-03-21 21:44:34 +0000

    add more ut

--- toolkit/log/log_test.go
+++ toolkit/log/log_test.go
@@ -7,11 +7,41 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/sirupsen/logrus"
 )
 
 var _ = Describe(
 	"Logger functionality",
 	func() {
+		Context("WithSpanInfo", func() {
+			var (
+				baseLogger *logrus.Logger
+				logger     *Logger
+			)
+
+			BeforeEach(func() {
+				baseLogger = logrus.New()
+				logger = &Logger{logger: logrus.NewEntry(baseLogger)}
+			})
+
+			BeforeEach(func() {
+				baseLogger = logrus.New()
+				logger = &Logger{logger: logrus.NewEntry(baseLogger)}
+			})
+
+			It("returns the same logger when context is nil", func() {
+				result := logger.withSpanInfo(nil)
+				Expect(result).To(BeIdenticalTo(logger))
+			})
+
+			It("returns the same logger when span is invalid", func() {
+				ctx := context.Background() // no span in context
+				result := logger.withSpanInfo(ctx)
+				Expect(result).To(BeIdenticalTo(logger))
+			})
+
+		})
+
 		It("NewLogger", func() {
 			logger := NewLogger(3)
 			ctx := logger.WithLogger(context.Background())
